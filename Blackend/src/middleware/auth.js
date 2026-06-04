8d24a8cee22967da706c7cc2befdf0d52891312f
Adisak Sawangpuk <adisak2545@gmail.com>
2026-06-04 12:35:32 +0700

    ลบ console.log ออก

--- Blackend/src/middleware/auth.js
+++ Blackend/src/middleware/auth.js
@@ -10,7 +10,6 @@ const auth = (req , res , next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
-        console.log(req.user);
         next();
     } catch (err) {
         return res.status(401).json({ error: 'Invalid token' });
