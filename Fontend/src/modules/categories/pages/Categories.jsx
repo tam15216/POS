256b1d9de8d353d4188ba05ad76eea322b362fae
Adisak Sawangpuk <adisak2545@gmail.com>
2026-06-05 14:14:50 +0700

    ปรับเพิ่ม Pagination

--- Fontend/src/modules/categories/pages/Categories.jsx
+++ Fontend/src/modules/categories/pages/Categories.jsx
@@ -2,21 +2,22 @@ import CategoryTable from "../components/CategotyTable";
 import CategoryForm from "../components/CategoryForm";
 import { useState } from "react";
 import useCategories from "../hooks/useCategories";
+import { usePagination } from "../../../shared/hooks/usePagination";
+import Pagination from "../../../shared/components/Pagination";
 
 export default function Categories() {
   const { loading, addCategory, toggleCategorys , categories } = useCategories();
   const [open, setOpen] = useState(false);
+  const categoriespagination = usePagination(categories, 10);
 
   return (
     <div className="p-6">
-      {/* Header */}
       <div className="flex items-center justify-between mb-6">
         <div>
           <h1 className="text-3xl font-bold text-purple-700">Categories</h1>
         </div>
       </div>
 
-      {/* Form */}
       <div className="mb-6">
         <button
           onClick={() => setOpen(true)}
@@ -26,21 +27,23 @@ export default function Categories() {
         </button>
       </div>
 
-      {/* Divider */}
       <div className="mb-6 border-b border-purple-100"></div>
 
-      {/* Table */}
       <div className="p-5 bg-white border border-purple-100 shadow-sm rounded-2xl">
         {loading ? (
           <div className="py-10 text-center text-gray-400">Loading...</div>
         ) : (
           <CategoryTable categories={categories} onDelete={toggleCategorys} />
         )}
+        <Pagination
+          currentPage={categoriespagination.currentPage}
+          totalPages={categoriespagination.totalPages}
+          onPageChange={categoriespagination.setCurrentPage}
+        />
       </div>
       {open && (
         <div className="fixed inset-0 z-50 flex items-center justify-center px-4 bg-black/40 backdrop-blur-sm">
           <div className="w-full max-w-md p-6 bg-white border border-purple-100 shadow-2xl rounded-2xl">
-            {/* Header */}
             <div className="flex items-center justify-between mb-6">
               <div>
                 <h2 className="text-2xl font-bold text-purple-700">
@@ -60,7 +63,6 @@ export default function Categories() {
               </button>
             </div>
 
-            {/* Form */}
             <CategoryForm
               onSubmit={async (data) => {
                 await addCategory(data);
